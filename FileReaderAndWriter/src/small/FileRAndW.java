eb783ae1dae7166646d189a0804040959c886408
candicemary <candice.carsia@outlook.com>
2020-10-07 01:05:44 +0930

    Read file

--- FileReaderAndWriter/src/small/FileRAndW.java
+++ FileReaderAndWriter/src/small/FileRAndW.java
@@ -1,10 +1,19 @@
 package small;
+import java.io.*;
 
 public class FileRAndW {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 System.out.println("Hello, World"); 
+		File file = new File("C:\\Users\\Candi\\Desktop\\JavaTest\\ReadandWrite\\ARead.txt");//创建一个File 
+		try (InputStream words = new FileInputStream(file)) {//从外部资源获将数据字节移动到java 程序中----取获取字节流
+			
+			byte[]data = new byte[1024];// 创建一个长度是1024的byte 类型数组，用来存储读取到的数据
+			
+			words.read(data);//读取到的数据存储在byte里 . words是指此次实际读取到的字节个数
+			System.out.println("File Context:" + new String(data));
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 }
