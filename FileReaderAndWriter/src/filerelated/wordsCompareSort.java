60fb6cf4e437e9935c0a986951fee234e5b7d3df
candicemary <candice.carsia@outlook.com>
2020-10-17 23:32:03 +0930

    Finding Median number

--- FileReaderAndWriter/src/filerelated/wordsCompareSort.java
+++ FileReaderAndWriter/src/filerelated/wordsCompareSort.java
@@ -6,6 +6,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class wordsCompareSort {
 
@@ -15,48 +18,71 @@ public class wordsCompareSort {
 			String pathName = "C:\\Users\\Candi\\Desktop\\JavaTest\\ReadandWrite\\BRead.txt";
 			File filename = new File(pathName);
 			// ArrayList list = new ArrayList()
+
 			InputStreamReader reader = new InputStreamReader(new FileInputStream(filename));
 			BufferedReader br = new BufferedReader(reader);
+			List<Integer> listE = new ArrayList<Integer>();
 			String str = "";
+			Integer inte = null;
 			int minh = 1000;
 			int maxh = 1000;
 			int minw = 1000;
 			int maxw = 1000;
-			String name ="";
-			
+			String name = "";
+
 			while ((str = br.readLine()) != null) {// line 一直读读到 line 为空为止
+				listE.add(str);
+				if (str != null) {
+					inte = Integer.valueOf(str);
+				}
 				if (str.startsWith("#")) {
 					continue;
 				}
 				String[] subStr = str.split("\t");
 				String height = subStr[1];
 				String weight = subStr[2];
-			
+
 				double h = Double.parseDouble(height);
 				double w = Double.parseDouble(weight);
 				if (minh > h) {
 					int ll = (int) h;
 					minh = ll;
+					name = subStr[0];
 				}
-				if(maxh<h) {
+				if (maxh < h) {
 					int ll = (int) h;
-					maxh =ll;
-					name =subStr[0];
-					
-				}
-				if(minw>w) {
-					int ww =(int)w;
-					minw
-					
-				}
-			
-			}
-			
-			br.close();
+					maxh = ll;
+					name = subStr[0];
 
-			File writeWords = new File("C:\\Users\\Candi\\Desktop\\JavaTest\\ReadandWrite\\output00.txt");
+				}
+				if (minw > w) {
+					int ww = (int) w;
+					minw = ww;
+					name = subStr[0];
+				}
+				if (maxw > w) {
+					int mw = (int) w;
+					maxw = mw;
+					name = subStr[0];
+				}
+
+			}
+
+			Collections.sort(listE);
+			int size = listE.size();
+			if (size % 2 == 1) {
+
+			}
+
+			File writeWords = new File("C:\\Users\\Candi\\Desktop\\JavaTest\\ReadandWrite\\output11.txt");
 			writeWords.createNewFile();
 			BufferedWriter out = new BufferedWriter(new FileWriter(writeWords));
+			String line = "";
+
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+				out.write(line);
+			}
 
 			out.close();
 		} catch (Exception e) {
