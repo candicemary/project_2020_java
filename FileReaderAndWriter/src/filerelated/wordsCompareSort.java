2837c23dc1ecb24dbe976e8264c54d97a0635d3d
candicemary <candice.carsia@outlook.com>
2020-10-23 19:16:43 +0930

    Median Number and sort table

--- FileReaderAndWriter/src/filerelated/wordsCompareSort.java
+++ FileReaderAndWriter/src/filerelated/wordsCompareSort.java
@@ -8,6 +8,7 @@ import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class wordsCompareSort {
@@ -21,84 +22,74 @@ public class wordsCompareSort {
 
 			InputStreamReader reader = new InputStreamReader(new FileInputStream(filename));
 			BufferedReader br = new BufferedReader(reader);
-			List<String> listE = new ArrayList<String>();
+			List<String[]> listE = new ArrayList<String[]>();
 			String str = "";
 			Integer inte = null;
-			int minh = 1000;
-			int maxh = 1000;
-			int minw = 1000;
-			int maxw = 1000;
+
 			String name = "";
 
-			while ((str = br.readLine()) != null) {// line 一直读读到 line 为空为止
-				listE.add(str);
-				if (str != null) {
-					inte = Integer.valueOf(str);
-				}
+			while ((str = br.readLine()) != null) {// line 一直读读到 line 为空为止 // String
 				if (str.startsWith("#")) {
+
 					continue;
 				}
 				String[] subStr = str.split("\t");
-				String height = subStr[1];
-				String weight = subStr[2];
-
-				double h = Double.parseDouble(height);
-				double w = Double.parseDouble(weight);
-				if (minh > h) {
-					int ll = (int) h;
-					minh = ll;
-					name = subStr[0];
-				}
-				if (maxh < h) {
-					int ll = (int) h;
-					maxh = ll;
-					name = subStr[0];
-
-				}
-				if (minw > w) {
-					int ww = (int) w;
-					minw = ww;
-					name = subStr[0];
-				}
-				if (maxw > w) {
-					int mw = (int) w;
-					maxw = mw;
-					name = subStr[0];
-				}
-
+				listE.add(subStr);
 			}
-			double s = 0;
-			Collections.sort(listE);
+
+			Collections.sort(listE, new Comparator<String[]>() {// 泛型、、定义list 类型string int double
+				public int compare(String[] o1, String[] o2) {// 任意的两个string
+
+					double H1 = Double.parseDouble(o1[1]);
+					double H2 = Double.parseDouble(o2[1]);
+
+					int com = (int) (H1 - H2);
+					return com;// 匿名内部类的目的-排序-冒泡法-两个元素谁大谁小
+				}
+			});
+
 			int size = listE.size();
+			double md = 0;
 			if (size % 2 == 1) {
-				
-				s = (listE.get(size/2-1) + listE.get(size/2)+0.0)/2;//加0.0是为了计算是将int类型转换为浮点型
-		    	
-		    }else {
-		    	s = listE.get((size-1)/2);
-		    }
-			return s;
-	
+				String[] midNumO = listE.get(size / 2 - 1);
+
+				// 自动转化成double// 从listE 里面拿到数组
+				md = Double.parseDouble(midNumO[1]);
+			} else {
+				String[] midNumO = listE.get(size / 2 - 1);
+				String[] midNumE = listE.get(size / 2);
+				double Mid2 = Double.parseDouble(midNumE[1]);
+				double Mid1 = Double.parseDouble(midNumO[1]);
+				md = (Mid1 + Mid2) / 2;
+
 			}
 
 			File writeWords = new File("C:\\Users\\Candi\\Desktop\\JavaTest\\ReadandWrite\\output11.txt");
 			writeWords.createNewFile();
 			BufferedWriter out = new BufferedWriter(new FileWriter(writeWords));
-			String line = "";
 
-			while ((line = br.readLine()) != null) {
-				System.out.println(line);
-				out.write(line);
+			out.write("中位数是：" + md + "\n\n");
+
+			for (int j = 0; j < listE.size(); j++) {
+				String[] listRes = listE.get(j);
+				String finalRes = "";
+				for (int i = 0; i < listRes.length; i++) {
+					if (i == 0) {
+						finalRes = listRes[i];
+					} else {
+						finalRes = finalRes + "\t" + listRes[i];
+					}
+				}
+				out.write(finalRes + "\n");
 			}
-
 			out.close();
-		}catch(
+		} catch (
 
-	Exception e)
-	{
-		System.out.println("抛出异常" + e);
-		e.printStackTrace();
+		Exception e) {
+			System.out.println("抛出异常" + e);
+			e.printStackTrace();
+
+		}
 
 	}
-
-}}
+}
